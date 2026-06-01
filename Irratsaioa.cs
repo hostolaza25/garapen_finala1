c52fd7bf633db981e0dc04f34742ac9449d7c1af
hostolaza25 <hostolaza25@izarraitz.eus>
2026-06-01 12:36:12 +0200

    Add files via upload

---
+++ Irratsaioa.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3POO_3
+{
+    /// <summary>
+    /// Irrati baten informazioa eta funtzionamendua kudeatzen duen klasea.
+    /// Frekuentzia eta irratiaren izena gordetzen ditu.
+    /// </summary>
+    class Irratsaioa
+    {
+        /// <summary>
+        /// Irratiaren frekuentzia gordetzen duen atributu pribatua.
+        /// </summary>
+        private double frek;
+
+        /// <summary>
+        /// Irratiaren izena gordetzen duen atributu pribatua.
+        /// </summary>
+        private string izena;
+
+        /// <summary>
+        /// Irratiaren frekuentzia lortu edo ezartzen du.
+        /// </summary>
+        public double Frek
+        {
+            get { return frek; }
+            set { frek = value; }
+        }
+
+        /// <summary>
+        /// Irratiaren izena lortu edo ezartzen du.
+        /// </summary>
+        public string Izena
+        {
+            get { return izena; }
+            set { izena = value; }
+        }
+
+        /// <summary>
+        /// Eraikitzaile lehenetsia.
+        /// Frekuentzia 80 MHz balioarekin hasieratzen du.
+        /// </summary>
+        public Irratsaioa()
+        {
+            frek = 80;
+        }
+
+        /// <summary>
+        /// Parametroak jasotzen dituen eraikitzailea.
+        /// </summary>
+        /// <param name="balorea">Hasierako frekuentzia.</param>
+        /// <param name="izena">Irratiaren izena.</param>
+        public Irratsaioa(double balorea, string izena)
+        {
+            frek = balorea;
+            this.izena = izena;
+        }
+
+        /// <summary>
+        /// Frekuentzia 0,5 MHz igotzen du.
+        /// 108 MHz baino handiagoa bada, 80 MHz-ra itzultzen da.
+        /// </summary>
+        public void igo()
+        {
+            frek = frek + 0.5;
+
+            if (frek > 108)
+            {
+                frek = 80;
+            }
+        }
+
+        /// <summary>
+        /// Frekuentzia 0,5 MHz jaisten du.
+        /// 80 MHz baino txikiagoa bada, 108 MHz-ra pasatzen da.
+        /// </summary>
+        public void jaitsi()
+        {
+            frek = frek - 0.5;
+
+            if (frek < 80)
+            {
+                frek = 108;
+            }
+        }
+
+        /// <summary>
+        /// Pantailan irratiaren frekuentzia eta izena erakusten ditu.
+        /// </summary>
+        public void seefrek()
+        {
+            Console.WriteLine("Frekuentzia = " + frek + " Irratsaio izena> " + izena);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _3POO_3
+{
+    /// <summary>
+    /// Programaren klase nagusia.
+    /// Aplikazioaren exekuzioa hemen hasten da.
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Programaren sarrera-puntua.
+        /// Irratsaioa klasearen objektuak sortu eta haien funtzionamendua probatzen du.
+        /// </summary>
+        /// <param name="args">Komando-lerroko argumentuak.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NIRE IRRATIA");
+
+            // EITB irratiaren objektua sortu
+            Irratsaioa eitb = new Irratsaioa(104.3, "eitb musika");
+
+            // KISS FM irratiaren objektua sortu (eraikitzaile lehenetsia erabiliz)
+            Irratsaioa kissfm = new Irratsaioa();
+
+            Console.Write("EITB >");
+            eitb.seefrek();
+
+            Console.Write("KISS FM >");
+            kissfm.seefrek();
+
+            // Frekuentzia jaitsi eta berriro erakutsi
+            kissfm.jaitsi();
+
+            Console.Write("KISS FM >");
+            kissfm.seefrek();
+        }
+    }
+}
